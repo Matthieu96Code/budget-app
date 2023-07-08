a5f3065b808f1173554e4ac4e0b6574c89259c93
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-08 12:07:37 +0000

    Update operations_controller.rb

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,5 +1,5 @@
 class OperationsController < ApplicationController
-  before_action :set_operation, only: %i[ show edit update destroy ]
+  before_action :set_operation, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
   # GET /operations or /operations.json
@@ -10,50 +10,51 @@ class OperationsController < ApplicationController
   end
 
   # GET /operations/1 or /operations/1.json
-  def show
-  end
+  def show; end
 
   # GET /operations/new
   def new
     @category_id = params[:category_id]
-    if params[:category_id]
-      @category_back = Category.find(params[:category_id])
-    else
-      @category_back = nil
-    end
     @operation = Operation.new
   end
 
   # GET /operations/1/edit
-  def edit
-  end
+  def edit; end
 
   def create
     category_ids = operation_params[:category_ids]
 
     if category_ids.blank?
-      @operation = Operation.new(operation_params.except(:category_ids))
-      @operation.author = current_user
-      @operation.category_id = nil
+      create_single_operation
     else
-      category_ids.each do |category_id|
-        @operation = Operation.new(operation_params.except(:category_ids))
-        @operation.category_id = category_id
-        
-        @operation.author = current_user
-        @operation.save
-      end
+      create_multiple_operations(category_ids)
     end
 
     respond_to do |format|
       if @operation.save
-        # format.html { redirect_to  category_operations_path(Category.find(params[:operation][:category_id])), notice: "Operation was successfully created." }
-        format.html { redirect_to  category_operations_path(Category.find(category_ids.first)), notice: "Operation was successfully created." }
-        format.json { render :show, status: :created, location: @operation }
+        format.html do
+          redirect_to category_operations_path(Category.find(category_ids.first)),
+                      notice: 'Operation was successfully created.'
+        end
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @operation.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  private
+
+  def create_single_operation
+    @operation = Operation.new(operation_params.except(:category_ids))
+    @operation.author = current_user
+  end
+
+  def create_multiple_operations(category_ids)
+    category_ids.each do |category_id|
+      @operation = Operation.new(operation_params.except(:category_ids))
+      @operation.category_id = category_id
+      @operation.author = current_user
+      @operation.save
     end
   end
 
@@ -61,8 +62,7 @@ class OperationsController < ApplicationController
   def update
     respond_to do |format|
       if @operation.update(operation_params)
-        format.html { redirect_to operation_url(@operation), notice: "Operation was successfully updated." }
-        format.json { render :show, status: :ok, location: @operation }
+        format.html { redirect_to operation_url(@operation), notice: 'Operation was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @operation.errors, status: :unprocessable_entity }
@@ -75,20 +75,19 @@ class OperationsController < ApplicationController
     @operation.destroy
 
     respond_to do |format|
-      format.html { redirect_to operations_url, notice: "Operation was successfully destroyed." }
+      format.html { redirect_to operations_url, notice: 'Operation was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_operation
-      @operation = Operation.find(params[:id])
-      @category_back = Category.find(params[:category_id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_operation
+    @operation = Operation.find(params[:id])
+    @category_back = Category.find(params[:category_id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def operation_params
-      params.require(:operation).permit(:name, :amount, category_ids: []) 
-    end
+  # Only allow a list of trusted parameters through.
+  def operation_params
+    params.require(:operation).permit(:name, :amount, category_ids: [])
+  end
 end
